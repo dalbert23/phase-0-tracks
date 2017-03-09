d4cdcbc357e28ad291695b4ef40027086c27d31d
David Albert <davidlyall2@gmail.com>
2017-03-08 22:23:55 -0500

    added user input questions

---
+++ ruby/hamsters.rb
@@ -0,0 +1,16 @@
+print "What is your hamsters name?"
+	name = gets.chomp
+print "How loud are the hamsters on a scale of 1-10?"
+	noise = gets.chomp
+print "What is the hamsters fur color?"	
+	color = gets.chomp
+print "Is the hamster a good candidate for adoption? (yes or no)"	
+    adopt = gets.chomp
+print "How old is the hamster in years?"    
+	age = gets.chomp
+	if age == nil  
+		puts age = nil
+	else
+	   puts "#{age}"	
+	end	
+
