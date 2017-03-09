c5f016f82f231d73bcae67098e7ac971ca80695b
David Albert <davidlyall2@gmail.com>
2017-03-08 22:29:39 -0500

    changed strings to proper data types

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,12 +2,14 @@ print "What is your hamsters name?"
 	name = gets.chomp
 print "How loud are the hamsters on a scale of 1-10?"
 	noise = gets.chomp
+	noise.to_i
 print "What is the hamsters fur color?"	
 	color = gets.chomp
 print "Is the hamster a good candidate for adoption? (yes or no)"	
     adopt = gets.chomp
 print "How old is the hamster in years?"    
 	age = gets.chomp
+	age.to_i
 	if age == nil  
 		puts age = nil
 	else
