785d3f2363c691fb7850c2bf2c0d96cb6cdf98e3
David Albert <davidlyall2@gmail.com>
2017-03-09 22:23:50 -0500

    added logic to check for vampires

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -9,13 +9,13 @@ print "Our company cafe has garlic bread. Should we order some for you?"
 print "Would you like to enroll in the company's health insurance?"
 	insurance = gets.chomp
 
-if age == true and (food == yes or insurance == yes)
+if age = true and (food = "yes" or insurance = "yes")
 	puts "Probably not a vampire"
-elsif age == false and (food == no or insurance == no)
+elsif age = false and (food = "no" or insurance = "no")
 	puts "Probably a vampire"
-elsif age == flase and food == no and insurance == no
+elsif age = false and food = "no" and insurance = "no"
 	puts "Almost certainly a vampire"
-elsif name == "Drake Cula" or name == "Tu Fang"
+elsif name = "Drake Cula" or name = "Tu Fang"
 	puts "Definitely a vampire"
 else 
 	puts "Results inconclusive"
